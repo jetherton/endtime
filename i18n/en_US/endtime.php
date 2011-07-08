5329b631a361aae5f1c4316c5ed138340c7b827b
carjimdra <carjimdra@gmail.com>
2011-07-08 16:36:31 +0000

    fixed a missing comma

--- i18n/en_US/endtime.php
+++ i18n/en_US/endtime.php
@@ -1,9 +1,18 @@
 <?php
-
+/**
+ *  End Time English Language file
+ *
+ * This plugin was written for Ushahidi Liberia, by the contractors of Ushahidi Liberia
+ * 2011
+ *
+ * @package  End Time plugin
+ * @author     Carter Draper <carjimdra@gmail.com>
+ * 
+ */
 
 $lang = array(
 'endtime' => 'End Time',
-'endtime_description' => 'time the event started'
+'endtime_description' => 'the time the event stopped',
 'applicable' => 'Applicable?',
-'applicable_description' => 'Is an end time applicable to this event?'
+'applicable_description' => 'Is an end time applicable to this event?',
 );

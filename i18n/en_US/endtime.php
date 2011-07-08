2d4e89a25f5964002882797d242eef42063ebd4e
carjimdra <carjimdra@gmail.com>
2011-07-08 16:32:32 +0000

    add english language file

---
+++ i18n/en_US/endtime.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$lang = array(
+'endtime' => 'End Time',
+'endtime_description' => 'time the event started'
+'applicable' => 'Applicable?',
+'applicable_description' => 'Is an end time applicable to this event?'
+);
